afa69104eb85d882de6197a76437ab68a7a5367f
battistaennio <ennio.battista99@gmail.com>
2024-07-11 16:01:29 +0200

    fix: change name of AppSearchBar in AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: "AppHeader",
+    data(){
+        return{
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" v-model="store.searchTxt">
+        <button @click="$emit('search')">cerca</button>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/general.scss" as *;
+@use "../style/partials/variables.scss" as *;
+</style>
