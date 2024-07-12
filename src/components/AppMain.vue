8814ab0402e98424d4d9c69188a4119a385ef858
battistaennio <ennio.battista99@gmail.com>
2024-07-12 10:38:38 +0200

    feat: add titles in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,10 +19,12 @@ export default {
 
 <template>
     <section>
-        <h2>Film:</h2>
+        <h2 v-if="store.movieList.length === 0 && store.TVseriesList.length === 0">Cosa hai voglia di guardare stasera?</h2>
+
+        <h2 v-if="store.movieList.length > 0">Films:</h2>
         <FilmCard v-for="(film, i) in store.movieList" :key="i" :details="film"/>
 
-        <h2>TV Series:</h2>
+        <h2 v-if="store.TVseriesList.length > 0">TV Series:</h2>
         <TVseriesCard v-for="series in store.TVseriesList" :info="series"/>
     </section>
 </template>
