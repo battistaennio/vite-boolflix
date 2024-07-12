155349676c17fb01c524e377ca168a84e839a25b
battistaennio <ennio.battista99@gmail.com>
2024-07-12 12:44:38 +0200

    style: AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,13 +12,47 @@ export default {
 </script>
 
 <template>
-    <div>
-        <input @keyup.enter="$emit('search')" type="text" v-model="store.searchTxt">
-        <button @click="$emit('search')">cerca</button>
-    </div>
+    <header>
+        <h1>boolflix</h1>
+        <div>
+            <input @keyup.enter="$emit('search')" type="text" v-model="store.searchTxt" placeholder="Inserisci il titolo">
+            <button @click="$emit('search')">cerca</button>
+        </div>
+    </header>
 </template>
 
 <style scoped lang="scss">
 @use "../style/general.scss" as *;
 @use "../style/partials/variables.scss" as *;
+
+header{
+    background-color: $secondary-background-color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+
+
+    h1{
+        color: $secondary-txt-color;
+        text-transform: uppercase;
+    }
+
+    input{
+       padding: 5px;
+       font-size: 15px;
+       border: none;
+       border-radius: 5px;
+    }
+
+    button{
+        padding: 2.5px 10px;
+        font-size: 15px;
+        border: none;
+        border-radius: 5px;
+        color: white;
+        background-color: $secondary-txt-color;
+        cursor: pointer;
+    }
+}
 </style>
