2e34041f512a7aa1ec3a88a27ec36e5af5063a05
battistaennio <ennio.battista99@gmail.com>
2024-07-15 14:27:49 +0200

    fix: language api response italian

--- src/store.js
+++ src/store.js
@@ -4,11 +4,11 @@ export const store = reactive({
     searchTxt: "",
 
     //film
-    apiURLfilm: "https://api.themoviedb.org/3/search/movie?api_key=d9509d0b5a3aea54142f22ed37d866b1&language=it_IT&query=",
+    apiURLfilm: "https://api.themoviedb.org/3/search/movie?api_key=d9509d0b5a3aea54142f22ed37d866b1&language=it&query=",
     movieList: [],
 
     //serie tv
-    apiURLtvSeries: "https://api.themoviedb.org/3/search/tv?api_key=d9509d0b5a3aea54142f22ed37d866b1&language=it_IT&query=",
+    apiURLtvSeries: "https://api.themoviedb.org/3/search/tv?api_key=d9509d0b5a3aea54142f22ed37d866b1&language=it&query=",
     TVseriesList: [],
 
 });
